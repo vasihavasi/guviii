aac6f9c7909a8255dc5912559d1e8aff2ba2b7ad
vasihavasi <42801745+vasihavasi@users.noreply.github.com>
2018-09-24 15:02:52 +0530

    Update and rename pos.py to odd.py

---
+++ odd.py
@@ -0,0 +1,5 @@
+number = int(input("enter the number " "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
--- pos.py
+++
@@ -1,7 +0,0 @@
-number = float(input(""))
-if number > 0:
-   print("Positive")
-elif number == 0:
-   print("Zero")
-else:
-   print("Negative")
